a5bcd6c2968ce81eabdd2ca59eb87ce599748e64
guilherme otsu <guiotsu@gmail.com>
2024-09-02 11:41:28 -0300

    add drag scroll on charybdis

--- keyboards/bastardkb/charybdis/3x5/keymaps/otsu/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/otsu/keymap.c
@@ -17,25 +17,25 @@
 
 #include QMK_KEYBOARD_H
 
-#define SP_NUM LT(_ARR, KC_SPC)
-#define Z_HOLD LT(_MOUSE,KC_Z)
-#define SL_HOLD LT(_MOUSE, KC_SLSH)
+#define SP_NUM LT(_NAV, KC_SPC)
+#define Z_MOUSE LT(_MOUSE,KC_Z)
+#define SL_FUN LT(_FUN, KC_SLSH)
 #define TB_TIL LT(_TIL, KC_TAB)
-#define ENT_NUM LT(_NUMBER, KC_ENT)
-#define BSP_SYN LT(_SYM, KC_BSPC)
+#define ENT_SYM LT(_SYM, KC_ENT)
+#define BSP_NUM LT(_NUMBER, KC_BSPC)
 
-#define GUI_A G(KC_A)
-#define GUI_S G(KC_SCLN)
-#define ALT_S A(KC_S)
-#define ALT_L A(KC_L)
-#define CTR_D C(KC_D)
-#define CTR_K C(KC_K)
-#define SFT_F S(KC_F)
-#define SFT_J S(KC_J)
+#define GUI_A LGUI_T(KC_A)
+#define GUI_S LGUI_T(KC_SCLN)
+#define ALT_S LALT_T(KC_S)
+#define ALT_L LALT_T(KC_L)
+#define CTR_D CTL_T(KC_D)
+#define CTR_K CTL_T(KC_K)
+#define SFT_F SFT_T(KC_F)
+#define SFT_J SFT_T(KC_J)
 
 enum charybdis_keymap_layers {
     _BASE = 0,
-    _ARR,
+    _NAV,
     _NUMBER,
     _MOUSE,
     _SYM,
@@ -53,43 +53,38 @@ enum custom_keycodes {
     TD_DOT
 };
 
-enum combos {
-    THUMB_ESC
-};
-
-const uint16_t PROGMEM thumb_combo[] = {SP_NUM, TB_TIL, COMBO_END};
-combo_t key_combos[] = {
-    [THUMB_ESC] = COMBO(thumb_combo, KC_ESC),
-};
-
-// // Function to handle tap dance actions
-// void td_dragscroll_finished(tap_dance_state_t *state, void *user_data) {
-//     if (state->count == 1 && !state->pressed) {
-//         // Single tap: send "."
-//         register_code16(KC_DOT);
-//     } else if (state->pressed) {
-//         // Hold: enable drag scroll
-//         charybdis_set_pointer_dragscroll_enabled(true);
-//     }
-// }
-//
-// void td_dragscroll_reset(tap_dance_state_t *state, void *user_data) {
-//     if (state->pressed) {
-//         // On release: disable drag scroll
-//         charybdis_set_pointer_dragscroll_enabled(false);
-//     }
-// }
-//
-// //Tap Dance Definitions
-// tap_dance_action_t tap_dance_actions[] = {
-//   //Tap once for "." , hold for enable Drag Scroll, release for disable
-//   [TD_DOT]  = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_dragscroll_finished, td_dragscroll_reset),
-// };
+void tg_dragscroll_layer(keyrecord_t *record, uint16_t key, uint16_t lay){
+    // Tap send the key
+    // Hold active layer and drag scroll
+    // Didn't do this with tap dance because I don't have memory enough
+    if (record->event.pressed) {
+        if(record->tap.count == 0) {
+            charybdis_set_pointer_dragscroll_enabled(true);
+            layer_on(lay);
+        } else {
+            register_code(key);
+        }
+    } else {
+        if (record->tap.count == 0) {
+            charybdis_set_pointer_dragscroll_enabled(false);
+            layer_off(lay);
+        } else {
+            unregister_code(key);
+        }
+    }
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  // charybdis_set_pointer_dragscroll_enabled(true);
   uint16_t press = XXXXXXX;
   switch(keycode) {
+      case SP_NUM:
+          press = KC_SPC;
+          tg_dragscroll_layer(record, press, _NAV);
+          return false;
+      case SL_FUN:
+          press = KC_SLSH;
+          tg_dragscroll_layer(record, press, _FUN);
+          return false;
       case TLG1:
           press = LALT(KC_1);
           break;
@@ -127,19 +122,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
           GUI_A,   ALT_S,   CTR_D,   SFT_F,   KC_G,       KC_H,    SFT_J,   CTR_K,   ALT_L,   GUI_S,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-          Z_HOLD,   KC_X,   KC_C,    KC_V,    KC_B,       KC_N,     KC_M,   KC_COMM, TD_DOT,  SL_HOLD,
+          Z_MOUSE,   KC_X,   KC_C,    KC_V,    KC_B,       KC_N,     KC_M,   KC_COMM, KC_DOT,  SL_FUN,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                         QK_REP,  SP_NUM,    TB_TIL,      ENT_NUM, BSP_SYN
+                         KC_ESC,  SP_NUM,    TB_TIL,      ENT_SYM, BSP_NUM
   //                   ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
-  [_ARR] = LAYOUT(
+  [_NAV] = LAYOUT(
   // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_HOME,    KC_PGDN, KC_PGUP, KC_END, XXXXXXX,
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    KC_PGDN, KC_PGUP, XXXXXXX, XXXXXXX,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
        KC_LGUI, KC_LALT, KC_LCTL, KC_RSFT, XXXXXXX,    KC_LEFT,    KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,    XXXXXXX,   MS_WHLD, MS_WHLU, XXXXXXX, XXXXXXX,
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,    XXXXXXX,   KC_HOME, KC_END, XXXXXXX, KC_BSLS,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
                          XXXXXXX, XXXXXXX, _______,    XXXXXXX, KC_DEL
   //                   ╰───────────────────────────╯ ╰──────────────────╯
@@ -149,11 +144,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
        XXXXXXX, KC_7, KC_8, KC_9, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       XXXXXXX, KC_4, KC_5, KC_6, XXXXXXX,             KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT, XXXXXXX,
+       KC_QUOT, KC_4, KC_5, KC_6, KC_EQL,             XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
        KC_TILD, KC_1, KC_2, KC_3, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                         KC_0,    XXXXXXX, XXXXXXX,    _______, XXXXXXX
+                         KC_0,    XXXXXXX, KC_MINS,    _______, XXXXXXX
   //                   ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
@@ -161,19 +156,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       XXXXXXX,   XXXXXXX, KC_LCTL, KC_RSFT, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       XXXXXXX, XXXXXXX, KC_LCTL, KC_RSFT, XXXXXXX,    MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, XXXXXXX,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       _______, KC_CUT, KC_COPY,  KC_PSTE, XXXXXXX,    XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, _______,
+       _______, KC_CUT, KC_COPY,  KC_PSTE, XXXXXXX,    XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                         XXXXXXX, MS_BTN1, MS_BTN2,    XXXXXXX, XXXXXXX
+                         MS_BTN3, MS_BTN1, MS_BTN2,    XXXXXXX, XXXXXXX
   //                   ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
   [_SYM] = LAYOUT(
   // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-       XXXXXXX, KC_AMPR, KC_ASTR, XXXXXXX, XXXXXXX,    XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX,
+       XXXXXXX, KC_AMPR, KC_ASTR, XXXXXXX, XXXXXXX,    XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       XXXXXXX, KC_DLR, KC_PERC, KC_CIRC, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       XXXXXXX, KC_DLR, KC_PERC, KC_CIRC, XXXXXXX,     XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
        KC_GRV, KC_EXLM, KC_AT,  KC_HASH, KC_PLUS,     XXXXXXX, KC_LCBR,  KC_RCBR, XXXXXXX, KC_BSLS,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
@@ -197,11 +192,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       XXXXXXX,   TLG4,     TLG5,    TLG6, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       XXXXXXX,   TLG4,     TLG5,    TLG6, XXXXXXX,    KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MUTE,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       XXXXXXX,   TLG1,     TLG2,    TLG3, XXXXXXX,    XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
+       XXXXXXX,   TLG1,     TLG2,    TLG3, XXXXXXX,    XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, S(KC_BSLS),
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                         XXXXXXX, XXXXXXX, _______,    XXXXXXX, XXXXXXX
+                         XXXXXXX, XXXXXXX, _______,    XXXXXXX, KC_MPLY
   //                   ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
@@ -218,43 +213,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ),
 };
 // clang-format on
-
-// tapdance keycodes
-// enum td_keycodes {
-//   ALT_LP // Our example key: `LALT` when held, `(` when tapped. Add additional keycodes for each tapdance.
-// };
-//
-// // define a type containing as many tapdance states as you need
-// typedef enum {
-//   SINGLE_TAP,
-//   SINGLE_HOLD
-// } td_state_t;
-//
-// // create a global instance of the tapdance state type
-// static td_state_t td_state;
-//
-// // function to determine the current tapdance state
-// int cur_dance (qk_tap_dance_state_t *state);
-//
-// // `finished` and `reset` functions for each tapdance keycode
-// void altlp_finished (qk_tap_dance_state_t *state, void *user_data);
-// void altlp_reset (qk_tap_dance_state_t *state, void *user_data);
-//
-// int cur_dance (qk_tap_dance_state_t *state) {
-//   if (state->count == 1) {
-//     if (state->interrupted || !state->pressed) { return SINGLE_TAP; }
-//     else { return SINGLE_HOLD; }
-//   }
-// }
-//
-// void altlp_finished (qk_tap_dance_state_t *state, void *user_data) {
-//   td_state = cur_dance(state);
-//   switch (td_state) {
-//     case SINGLE_TAP:
-//       register_code16(KC_LPRN);
-//       break;
-//     case SINGLE_HOLD:
-//       register_mods(MOD_BIT(KC_LALT)); // for a layer-tap key, use `layer_on(_MY_LAYER)` here
-//       break;
-//   }
-// }
